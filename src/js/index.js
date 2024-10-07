477b8a64f8fe54b1fd4f1b8a486af1e878c638c3
lolekmakso <makstarasovv1@gmail.com>
2024-10-07 23:07:59 +0300

    added header and discover

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import autoCompleteFunc from "./modules/autoComplete";
+// autoCompleteFunc()
+
+// import { easepick } from '@easepick/bundle';
+import { easepick } from '@easepick/core';
+import { TimePlugin } from '@easepick/time-plugin';
+
+const picker = new easepick.create({
+    element: document.getElementById('datePicker'),
+    css: [
+      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+      'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
+    ],
+    zIndex: 10,
+    format: "HH:mm, DD/MM/YY",
+    plugins: [
+        TimePlugin
+    ]
+  });
+
