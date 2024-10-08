e98a8be4b2cddfdec4ff06d609dacb3c110aa39a
lolekmakso <makstarasovv1@gmail.com>
2024-10-08 22:12:21 +0300

    added swiper

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,17 @@
 // import easepickFun from "./modules/easepick";
 // easepickFun ()
 
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 32,
+
+  
+    navigation: {
+      nextEl: '#sliderNext',
+      prevEl: '#sliderPrev',
+    },
+  });
